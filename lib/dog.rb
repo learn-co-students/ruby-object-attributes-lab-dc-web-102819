e010739a8518b5a793d1fd8ab9f278f18a636219
jdromero88 <jdromerov88@gmail.com>
2019-10-31 01:26:44 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,14 @@
+class Dog
+  def name # getter
+    @name
+  end
+  def name=(name) # setter
+    @name = name
+  end
+  def breed
+    @breed
+  end
+  def breed=(breed)
+    @breed = breed
+  end
+end
